2a16e3d074d103f2677e91f5fa9cfab44e390672
arjun-the-cool <80453159+arjun-the-cool@users.noreply.github.com>
2021-07-01 21:16:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+menu_list_array = ["Veg Margherita Pizza","Farm Fresh Pizza","Simply Veg Pizza","Pepperoni Pizza","Chicken Pizza","Mutton Delight Pizza","Four Cheese Pizza","Chicken Tandoori Pizza","Veg Supreme Pizza","Veg Extraveganza Pizza","Deluxe Veggie Pizza"
+                    ];
+
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for(i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata + '<li>' + menu_list_array[i] + '<li>'
+}
+htmldata=htmldata + '<ol>'
+document.getElementById("display_menu").innerHTML=htmldata
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort()
+htmldata="<section class='cards'>"
+for(i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+"<div class='card'>"+"<img src'pizza.jpg'>" + menu_list_array[i] + "</div>"
+}
+htmldata=htmldata + "</section>"
+document.getElementById("display_addedmenu").innerHTML=htmldata;
+}
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    add_item()
+}
